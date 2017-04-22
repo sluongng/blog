e2c3e3e1cc247fd2a8bbce8f2cca94ff06a6740d
Son Luong Ngoc <sluongng@gmail.com>
2017-04-22 22:08:42 +0700

    Add skeleton for config.js file

---
+++ app/src/config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by NB on 4/19/2017.
+ */
+
+//Skeleton file only
+//TODO: Config information here
+
+export default {
+  MAX_ATTACHMENT_SIZE: 4000000,
+  apiGateway: {
+    URL: '',
+  },
+  s3: {
+    BUCKET: ''
+  },
+  cognito: {
+    REGION: '',
+    IDENTITY_POOL_ID: '',
+    USER_POOL_ID: '',
+    APP_CLIENT_ID: '',
+  }
+};
